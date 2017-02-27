6a813babccc1be66a7354a52c63349a3ef267101
Sergei <sleepytomcat@gmail.com>
2017-02-26 16:20:29 -0800

    Added thread functor object.

--- basic-threads/main.cpp
+++ basic-threads/main.cpp
@@ -2,7 +2,6 @@
 #include <thread>
 #include <sstream>
 
-
 class MultithreadedConsoleWriter {
 public:
     void writeLine(const std::string &text) {
@@ -16,21 +15,37 @@ private:
 
 MultithreadedConsoleWriter threadSafeConsoleWriter;
 
-void threadFunction(int tid) {
+void threadFunction() {
     std::stringstream textStream;
-    textStream << "Thread function launched by thread " << tid;
+    textStream << "Thread function launched by thread " << std::this_thread::get_id();
     threadSafeConsoleWriter.writeLine(textStream.str());
 }
+
+class ThreadFunctionObject {
+public:
+    ThreadFunctionObject(MultithreadedConsoleWriter &consoleWriter): console(consoleWriter) {}
+    void operator() () {
+        std::stringstream textStream;
+        textStream << "Thread functor launched by thread " << std::this_thread::get_id();
+        console.writeLine(textStream.str());
+    }
+
+private:
+    MultithreadedConsoleWriter &console;
+};
 
 int main() {
     std::thread threads[10];
     
-    //Launch a group of threads
-    for (int i = 0; i < 10; ++i) {
-        threads[i] = std::thread(threadFunction, i);
+    // Launch a group of threads with thread functions
+    for (int i = 0; i <= 5; ++i) {
+        threads[i] = std::thread(threadFunction);
     }
-    
-    threadSafeConsoleWriter.writeLine("Writing text from the main()");
+
+    // Launch a group of threads with thread functors
+    for (int i = 6; i < 10; ++i) {
+        threads[i] = std::thread(ThreadFunctionObject(threadSafeConsoleWriter));
+    }
     
     // Waiting for threads to complete
     for (int i = 0; i < 10; ++i) {
